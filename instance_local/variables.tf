10e1d144dc781aa1cc42265f46bf1542c34188c4
cssluism <css_luism@hotmail.com>
2021-11-12 00:02:29 -0400

    Creacion de Modulo

---
+++ instance_local/variables.tf
@@ -0,0 +1,31 @@
+
+variable "ami_id" {
+}
+variable "instance_type" {
+  
+}
+
+/* variable "autoscaling_name" {
+}
+variable "min_size" {
+}
+variable "max_size" {
+}
+variable "availability_zones" {
+  
+}
+variable "name_prefix" {
+}
+ */
+
+
+variable "tags" {}
+variable "sg_name" {}
+
+variable "ingress_rules" {}
+
+
+
+
+
+
